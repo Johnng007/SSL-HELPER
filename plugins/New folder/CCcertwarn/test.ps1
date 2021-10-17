5aa1ed13626093d3998cfcf589bf51bd441de48d
Ebuka John Onyejegbu <30580457+Johnng007@users.noreply.github.com>
2021-10-17 01:52:32 +0100

    Delete plugins/New folder directory

--- plugins/New folder/CCcertwarn/test.ps1
+++
@@ -1,9 +0,0 @@
-[Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
-$url = "https://john.ng/"
-$req = [Net.HttpWebRequest]::Create($url)
-$req.GetResponse() | Out-Null
-$output = [PSCustomObject]@{
-   URL = $url
-   'Cert Start Date' = $req.ServicePoint.Certificate.GetEffectiveDateString()
-   'Cert End Date' = $req.ServicePoint.Certificate.GetExpirationDateString()
-}
